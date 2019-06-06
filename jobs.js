4c22ec5a52b2f26b16254de022da0ae97a14244f
somme <sommes.email@gmail.com>
2019-06-06 12:33:58 +0100

    added new job, changed indel layout, updated logos and general styling

--- jobs.js
+++ jobs.js
@@ -8,43 +8,43 @@ module.exports = [
         hours: 'Full-time',
         location: 'London',
         role: `Reporting to the Platform Lead, the Site Reliability Engineer (SRE) will be part of a fast
-moving and highly technical team working on central platforms to support the software
-development life cycle. The SRE will work within a central function dedicated to improving a
-platform built around self service for developers, and will care deeply about building reliable,
-well-tested systems, collaboratively with others.`,
+          moving and highly technical team working on central platforms to support the software
+          development life cycle. The SRE will work within a central function dedicated to improving a
+          platform built around self service for developers, and will care deeply about building reliable,
+          well-tested systems, collaboratively with others.`,
         keyResponsibilities: [
-            'Enhance operational reliability and scalability of existing products',
-            'POC\'ing new ideas and products, and balancing trade offs between technical, analytical and product needs.',
-            'Streamlining operational tasks through engineering and automation',
-            'Improving operational visibility around core products such as tracking technologies, improved metrics etc.',
-            'Implementing integrations between components to improve the teams way of working i.e. slack, opsgenie and status pages integrations',
-            'Identify simple innovative technical solutions to complex engineering problems',
-            'Improve knowledge amongst the team through code reviews, documentation, technical guidance and mentoring.',
-            'Collaborate with product engineers regarding technical implementation and product improvements',
-            'Reduce the number of DevOps resources in projects by enabling developers to be more self-service.'
+          'Enhance operational reliability and scalability of existing products',
+          'POC\'ing new ideas and products, and balancing trade offs between technical, analytical and product needs.',
+          'Streamlining operational tasks through engineering and automation',
+          'Improving operational visibility around core products such as tracking technologies, improved metrics etc.',
+          'Implementing integrations between components to improve the teams way of working i.e. slack, opsgenie and status pages integrations',
+          'Identify simple innovative technical solutions to complex engineering problems',
+          'Improve knowledge amongst the team through code reviews, documentation, technical guidance and mentoring.',
+          'Collaborate with product engineers regarding technical implementation and product improvements',
+          'Reduce the number of DevOps resources in projects by enabling developers to be more self-service.'
         ],
         desirableAttributes: [
-            'Good understanding of industry best practices',
-            'Proven track record of developing APIs',
-            'A passion for new technology and automation',
-            'Enjoys solving difficult technical problems',
-            'Good knowledge of continuous integration and deployment practices',
-            'Great collaboration skills with people of all levels of technical ability',
-            'An understanding of the importance of being user driven and the benefits of DevOps principles, agile ways of working, collaboration and transparency',
-            'Software development experience in one or more general purpose programming languages',
-            'Experience with building web applications using frameworks like Angular, React or Ember',
-            'Extensive knowledge of container based technologies (Docker / Kubernetes)',
-            'Experience with cloud computing services (AWS or GCP)',
-            'Fantastic communication skills, both written and verbal.',
-            'Good software engineering principles',
-            'Experience on building decoupled services with micro-service architecture'
+          'Good understanding of industry best practices',
+          'Proven track record of developing APIs',
+          'A passion for new technology and automation',
+          'Enjoys solving difficult technical problems',
+          'Good knowledge of continuous integration and deployment practices',
+          'Great collaboration skills with people of all levels of technical ability',
+          'An understanding of the importance of being user driven and the benefits of DevOps principles, agile ways of working, collaboration and transparency',
+          'Software development experience in one or more general purpose programming languages',
+          'Experience with building web applications using frameworks like Angular, React or Ember',
+          'Extensive knowledge of container based technologies (Docker / Kubernetes)',
+          'Experience with cloud computing services (AWS or GCP)',
+          'Fantastic communication skills, both written and verbal.',
+          'Good software engineering principles',
+          'Experience on building decoupled services with micro-service architecture'
         ],
         remuneration: [
-            'Competitive salary',
-            '25 Days Holiday (plus bank holidays)',
-            'Flexible working hours',
-            'Performance Related Bonus',
-            'Pension'
+          'Competitive salary',
+          '25 Days Holiday (plus bank holidays)',
+          'Flexible working hours',
+          'Performance Related Bonus',
+          'Pension'
         ],
         securityClearanceRequired: true
     },
@@ -57,55 +57,55 @@ well-tested systems, collaboratively with others.`,
         hours: 'Full-time',
         location: 'London',
         role: `We are looking for someone to join a new growing engineering company as a Site Reliability
-Engineering Lead (SRE Lead), who has exceptional customer and people management skills.
-The Site Reliability Engineering Lead will be highly technical and passionate about technology,
-cloud, automation and engineering. They will understand engineering principles, development
-best practices and have an eye for detail around driving product and service metrics that we
-provide to customers.
-<br/>
-<br/>
-The SRE Lead will possess an intellectual curiosity around technical problems and have a
-desire for problem solving. They will deconstruct problems and utilise the teams diverse skill
-set to explore multiple opportunities for the right outcome. We encourage self-direction and will
-support individuals to explore alternative solutions to help drive a healthy culture of innovation.
-<br/>
-<br/>
-The SRE Lead will think of innovative ways to meet customer service requirements that
-challenge standard ITIL principles and collaborate with the engineering function to drive those
-changes through our products. You will also be responsible for guiding and mentoring the junior
-and senior engineers through their career development inline with the engineering strategy, and
-their career progression in an engineering function.`,
+          Engineering Lead (SRE Lead), who has exceptional customer and people management skills.
+          The Site Reliability Engineering Lead will be highly technical and passionate about technology,
+          cloud, automation and engineering. They will understand engineering principles, development
+          best practices and have an eye for detail around driving product and service metrics that we
+          provide to customers.
+          <br/>
+          <br/>
+          The SRE Lead will possess an intellectual curiosity around technical problems and have a
+          desire for problem solving. They will deconstruct problems and utilise the teams diverse skill
+          set to explore multiple opportunities for the right outcome. We encourage self-direction and will
+          support individuals to explore alternative solutions to help drive a healthy culture of innovation.
+          <br/>
+          <br/>
+          The SRE Lead will think of innovative ways to meet customer service requirements that
+          challenge standard ITIL principles and collaborate with the engineering function to drive those
+          changes through our products. You will also be responsible for guiding and mentoring the junior
+          and senior engineers through their career development inline with the engineering strategy, and
+          their career progression in an engineering function.`,
         keyResponsibilities: [
-            'Lead a team of engineers to deliver a reliable, performant set of customer products',
-            'Ensure a positive customer experience through feedback and client interactions',
-            'Define and lead strategy and roadmap in relation to business needs and priorities',
-            'Manage individual streams of priorities, deadlines and deliverables with your technical knowledge',
-            'Manage end-to-end performance, reliability and scalability for client critical platform components',
-            'Drive strategic operational improvements to reduce operational overheads within the organisation',
-            'Manage and maintain the support of multiple platform components',
-            'Be responsible for identifying improvements through automated integrations between systems',
-            'Be responsible for designing, engineering and implementing availability, reliability and scalability improvements',
-            'Manage third-party relationships during support issues as well as stakeholder management',
-            'Challenge ways of working, improve SLAs and metrics, and issue transparency to customers.',
-            'Responsible for guiding and mentoring junior and senior engineers through their career development'
+          'Lead a team of engineers to deliver a reliable, performant set of customer products',
+          'Ensure a positive customer experience through feedback and client interactions',
+          'Define and lead strategy and roadmap in relation to business needs and priorities',
+          'Manage individual streams of priorities, deadlines and deliverables with your technical knowledge',
+          'Manage end-to-end performance, reliability and scalability for client critical platform components',
+          'Drive strategic operational improvements to reduce operational overheads within the organisation',
+          'Manage and maintain the support of multiple platform components',
+          'Be responsible for identifying improvements through automated integrations between systems',
+          'Be responsible for designing, engineering and implementing availability, reliability and scalability improvements',
+          'Manage third-party relationships during support issues as well as stakeholder management',
+          'Challenge ways of working, improve SLAs and metrics, and issue transparency to customers.',
+          'Responsible for guiding and mentoring junior and senior engineers through their career development'
         ],
         desirableAttributes: [
-            'Good understanding of industry best practice',
-            'A passion for new technology and automation',
-            'Enjoys working with teams on resolving difficult technical problems',
-            'Good knowledge of continuous integration and deployment practices',
-            'Great collaboration skills with people of all levels of technical ability',
-            'Great culture and team building capabilities',
-            'Fantastic communication skills, both written and verbal'
+          'Good understanding of industry best practice',
+          'A passion for new technology and automation',
+          'Enjoys working with teams on resolving difficult technical problems',
+          'Good knowledge of continuous integration and deployment practices',
+          'Great collaboration skills with people of all levels of technical ability',
+          'Great culture and team building capabilities',
+          'Fantastic communication skills, both written and verbal'
         ],
         remuneration: [
-            'Competitive salary',
-            '25 Days Holiday (plus bank holidays)',
-            'On call allowance',
-            'Flexible working hours',
-            'Training and personal development',
-            'Pension',
-            'Performance Related Bonus'
+          'Competitive salary',
+          '25 Days Holiday (plus bank holidays)',
+          'On call allowance',
+          'Flexible working hours',
+          'Training and personal development',
+          'Pension',
+          'Performance Related Bonus'
         ],
         securityClearanceRequired: true
     },
@@ -118,14 +118,14 @@ their career progression in an engineering function.`,
         hours: 'Full-time',
         location: 'London',
         role: `We are looking for someone to join the company in the newly created role of Senior Technical
-Copywriter. You will have an opportunity to grow the role, and will have freedom and autonomy
-for creating a consistent image for the company. This position offers a great opportunity for
-the right candidate to build a small team to help drive the copy / content side of the business.
-We recognise the importance of good communication and welcome new ideas, improvements
-and change.<br /><br />
-You will work closely with product teams, engineers, marketing and business leaders to
-translate complex technical and business terms into tangible easily understandable concepts
-for users.`,
+          Copywriter. You will have an opportunity to grow the role, and will have freedom and autonomy
+          for creating a consistent image for the company. This position offers a great opportunity for
+          the right candidate to build a small team to help drive the copy / content side of the business.
+          We recognise the importance of good communication and welcome new ideas, improvements
+          and change.<br /><br />
+          You will work closely with product teams, engineers, marketing and business leaders to
+          translate complex technical and business terms into tangible easily understandable concepts
+          for users.`,
         keyResponsibilities: [
           'Work with product teams to take complex technical information and make it clear and comprehensible to product users',
           'Write technical documentation to support the build and development processes of the products',
@@ -151,6 +151,63 @@ for users.`,
             'Training and personal development',
             'Pension',
             'Performance Related Bonus'
+        ],
+        securityClearanceRequired: false
+    },
+    {
+        title: 'Senior Product Manager',
+        slug: 'senior-product-manager',
+        active: true,
+        postedDate: '6th Jun 2019',
+        type: 'Permanent',
+        hours: 'Full-time',
+        location: 'London',
+        role: `We are looking for someone to join a new growing product engineering company as a Senior
+          Product Manager. The successful candidate will be a technically experienced product manager
+          with enthusiasm for innovation, owning complex business problems, and the ambition to solve
+          these with products.<br /><br />
+          The Senior Product Manager will take ownership of one of the core products within the business
+          and help drive and shape its direction. As the number of products the Company provides
+          increases, so too will the scope of the position and its responsibilities.`,
+        keyResponsibilities: [
+          'Drive and communicate the vision of the product',
+          'Build a clear effective roadmap and help prioritise features using customer feedback and sales data provided',
+          'Liaise with customers and proactively engage on building a customer user profile to work with',
+          'Use leading questions and test theories to identify the root of the customers problem',
+          'Constantly improve the digital experience for the customer, both internal and external',
+          'Take complex feature proposals and customer issues and deliver them to the appropriate team in a concise and understandable way, breaking key areas down into manageable tasks',
+          'Maintain a vision for the product in relation to the fast-paced industry and its positioning',
+          'Contribute to marketing material and blogs',
+          'Confidently perform demonstrations',
+          'Help build the right team necessary to drive the product value and work closely with Engineering, Content, Marketing and Sales',
+          'Measure the success of the product in relation to its value proposition',
+          'Manage the full product lifecycle',
+          'Perform regular show and tells to the company on product progress, sharing insights and ideas',
+          'Be proactive in sharing and sponsoring new product ideas with the business and collaborating with stakeholders',
+          'Understand the market product fit and why',
+          'Understand the pain points of the problem domain and why it exists',
+          'Be proactive in using and understanding new technologies and why they exist, (what industry problems they are trying to solve)',
+          'Have exposure to pricing strategies and help contribute to these'
+        ],
+        desirableAttributes: [
+          'Extensive product management experience',
+          'Track record of delivering digital products from start to finish',
+          'Excellent organisational and analytical skills',
+          'Excellent communication and presentation skills',
+          'Deep understanding of agile delivery principles and engineering best practices',
+          'A development or engineering background preferable',
+          'Ability to understand how to translate business problems into technical solutions',
+          'Bachelor degree or equivalent experience',
+          'Be knowledgeable about Git, Git workflows and developer ways of working',
+          'Be passionate about design and usability'
+        ],
+        remuneration: [
+          'Competitive salary',
+          '25 Days Holiday (plus bank holidays)',
+          'Flexible working hours',
+          'Pension',
+          'Health Insurance',
+          'Performance Related Bonus'
         ],
         securityClearanceRequired: false
     }
