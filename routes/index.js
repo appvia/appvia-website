53d914770e797b0383b51448f6704e6f0202e59f
David Thompson <davethompson21@gmail.com>
2019-07-18 15:22:15 +0100

    Refactoring hub-demo and email-template routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
-var salesforce = require('../modules/salesforce');
-var gform = require('../modules/gform');
-var slack = require('../modules/slack');
 var Parser = require('rss-parser');
 var parser = new Parser();
 var router = express.Router();
 
 const hubDemoEnabled = process.env.HUB_DEMO_ENABLED === 'true';
-const emailAdminEnabled = process.env.EMAIL_ADMIN_ENABLED === 'true';
 const jobs = require('../jobs').filter(j => j.active);
 
 async function getBlogFeed() {
@@ -51,80 +47,5 @@ jobs.forEach(job => {
 router.get('/contact', function (req, res) {
   res.render('contact.html', {title: 'Appvia: Contact'});
 });
-
-if (emailAdminEnabled) {
-  router.get('/marketing-email-template', function (req, res) {
-    res.render('email-templates/marketing-email-template.html', {title: 'Appvia: Marketing Email Template',  firstName: req.query.firstName, lastName: req.query.lastName,  cta: req.query.cta});
-  });
-
-  router.get('/email-template', function (req, res) {
-    res.render('email-templates/email-template.html', {title: 'Appvia: Email Template',  firstName: req.query.firstName, lastName: req.query.lastName,   cta: req.query.cta});
-  });
-}
-
-if (hubDemoEnabled) {
-    router.get('/products/hub-demo', function (req, res) {
-        res.render('hub-demo/demo.html', {
-            title: 'Appvia: Request a Hub Demo',
-            slug: req.query.slug,
-            firstName: req.query.firstName,
-            lastName: req.query.lastName,
-            email: req.query.email,
-            companyName: req.query.companyName,
-            companySize: req.query.companySize,
-            role: req.query.role,
-            github: req.query.github,
-            errors: req.query.errors
-        });
-    });
-
-    router.get('/products/request-submit', function (req, res) {
-        res.render('hub-demo/request-submit.html', {title: 'Appvia: Thank you for your request'});
-    });
-
-    router.post('/products/request-submit', async function (req, res) {
-        console.log('Data submitted:', req.body);
-        try {
-            await gform.addContact(req.body);
-            const sfContact = await salesforce.isContact(req.body);
-            if (sfContact) {
-                await slack.message(
-                    process.env.SLACK_DEMOS_URL,
-                    `New demo creation required for: ${req.body.email}`,
-                    `*Qualified Customer* please create a new demo for ${req.body.email} at ${req.body.companyName}`
-                );
-                res.redirect('/products/request-submit');
-            } else {
-                res.redirect('/products/request-submit-pending');
-            }
-        } catch (err) {
-            console.log('Demo request failed:', err);
-            res.render('error.html', {
-                title: "Oops, sorry",
-                message: "Oops, sorry, error recording details",
-                status: err.status,
-                html_class: 'error',
-                error: {}
-            });
-        }
-    });
-
-    // Not a contact, but in form - we'll get back to them:
-    router.get('/products/request-submit-pending', function (req, res) {
-        res.render('hub-demo/request-submit-pending.html', {title: 'Appvia: Request Pending'});
-    });
-
-    router.get('/products/hub-demo/my-demo', function (req, res) {
-        res.render('hub-demo/my-demo.html', {title: 'Appvia: My Demo', email: req.query.email});
-    });
-
-    router.get('/products/hub-demo/integration-setup-admin-pages', function (req, res) {
-        res.render('hub-demo/integration-setup-admin-pages.html', {title: 'Appvia: Integration Setup Admin Pages'});
-    });
-
-    router.get('/products/hub-demo/feedback', function (req, res) {
-      res.render('hub-demo/feedback.html', {title: 'Appvia: Hub Demo Feedback' });
-    });
-}
 
 module.exports = router;
