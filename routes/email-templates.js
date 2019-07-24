1ce9608a33d43c42de6390d7b52dc74afc4bd2c0
Lewis Marshall <lewis.marshall@appvia.io>
2019-07-24 11:53:44 +0100

    renamed email template for kubecon

--- routes/email-templates.js
+++ routes/email-templates.js
@@ -4,8 +4,8 @@ router.get('/marketing-email-template', function (req, res) {
   res.render('email-templates/marketing-email-template.html', {title: 'Appvia: Marketing Email Template',  firstName: req.query.firstName, lastName: req.query.lastName,  cta: req.query.cta});
 });
 
-router.get('/email-template', function (req, res) {
-  res.render('email-templates/email-template.html', {title: 'Appvia: Email Template',  firstName: req.query.firstName, lastName: req.query.lastName,   cta: req.query.cta});
+router.get('/email-kubecon-demo', function (req, res) {
+  res.render('email-templates/email-kubecon-demo.html', {title: 'Appvia: Email Template',  firstName: req.query.firstName, cta: req.query.cta});
 });
 
 module.exports = router;
