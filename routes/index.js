23baef5f528b40431283cc04bcdc49cb51b518d7
somme <sommes.email@gmail.com>
2019-07-30 14:57:46 +0100

    started new website

--- routes/index.js
+++ routes/index.js
@@ -52,4 +52,8 @@ router.get('/privacy-policy', function (req, res) {
   res.render('privacy-policy.html', {title: 'Appvia: Privacy Policy'});
 });
 
+router.get('/new', function (req, res) {
+  res.render('new.html', {title: 'Appvia'});
+});
+
 module.exports = router;
