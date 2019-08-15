9bc8c68fa32b295ef5f10f90359154f4d36d4f4f
somme <sommes.email@gmail.com>
2019-08-15 16:21:07 +0100

    add acoridon css and breadcrumbs

---
+++ public/javascript/form.js
@@ -0,0 +1,25 @@
+Parsley.addValidator('git', {
+  validateString: function(value) {
+    return $.ajax('https://api.github.com/users/' + value)
+  },
+  messages: {en: 'Must be a valid GitHub ID'}
+});
+
+$('.conditionals').change(function() {
+  if(this.checked) {
+    $(this).closest('.form-check').find('.conditional-inputs').slideDown();
+    $(this).closest('.form-check').find('.conditional-inputs :input').prop('required', true);
+  }
+  else{
+    $(this).closest('.form-check').find('.conditional-inputs').slideUp();
+    $(this).closest('.form-check').find('.conditional-inputs :input').prop('required', false).val('');
+  }
+});
+
+$.listen('parsley:form:validated', function(e){
+  if (e.validationResult) {
+    $('button[type=submit]').attr('disabled', 'disabled').
+      prepend('<i class="fas fa-spinner fa-pulse mr-2"></i>').
+        find('span').text('Submitting...');
+  }
+});
---
+++ public/javascript/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#menu a').click(function(){
+    $('#menuToggle input:checkbox').prop('checked', false);
+  });
+});
