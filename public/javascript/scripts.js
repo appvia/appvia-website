eae5fcf35bbe610552d3c7dad95cab4d78f097b6
somme <sommes.email@gmail.com>
2019-08-16 14:17:45 +0100

    added active menu states on scroll

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-  $('#menu a').click(function(){
-    $('#menuToggle input:checkbox').prop('checked', false);
+$('#menu a').click(function(){
+  $('#menuToggle input:checkbox').prop('checked', false);
+});
+
+$('.parallax').scroll(function() {
+  $('.target').each(function() {
+      if($(window).scrollTop() >= $(this).offset().top) {
+        var id = $(this).attr('id');
+        $('#mainMenu a').removeClass('active');
+        $('#mainMenu a[href*='+ id +']').addClass('active');
+      }
   });
 });
