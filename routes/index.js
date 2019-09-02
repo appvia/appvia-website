55cfaedb10efab181dc1d5dbfecf4dde70962412
somme <sommes.email@gmail.com>
2019-09-02 16:55:40 +0100

    wip

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,33 @@ router.get('/', function (req, res) {
   res.render('index.html', {title: 'Appvia: Home', hubDemoEnabled});
 });
 
+router.get('/why-appvia', function (req, res) {
+  res.render('why-appvia.html', {title: 'Appvia: Why Appvia'});
+});
+
+router.get('/products', function (req, res) {
+  res.render('products.html', {title: 'Appvia: Products'});
+});
+
+router.get('/products/hub', function (req, res) {
+  res.render('hub.html', {title: 'Appvia: Hub'});
+});
+
+router.get('/products/product', function (req, res) {
+  res.render('product.html', {title: 'Appvia: Product'});
+});
+
+
 router.get('/blog', async function (req, res) {
   res.render('blog.html', {title: 'Appvia: Blog', rss: await getBlogFeed(), hubDemoEnabled});
 });
 
 router.get('/careers', function (req, res) {
   res.render('careers.html', {title: 'Appvia: Careers', jobCount: jobs.length, jobs, hubDemoEnabled});
+});
+
+router.get('/contact-us', function (req, res) {
+  res.render('contact-us.html', {title: 'Appvia: Contact Us'});
 });
 
 jobs.forEach(job => {
