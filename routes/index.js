5f977f124caa7a504273c6ad5a886d4edc949578
Alecsa <sommes.email@gmail.com>
2019-09-12 17:25:44 +0100

    added story bloks and content

--- routes/index.js
+++ routes/index.js
@@ -8,39 +8,22 @@ const hubDemoEnabled = process.env.HUB_DEMO_ENABLED === 'true';
 
 const jobs = require('../jobs').filter(j => j.active);
 
+const apiBase = 'https://api.storyblok.com/v1/cdn/';
+const token = 'pktLzISg00u5e33rrefWPgtt';
 
-async function getBlogFeed() {
-  try {
-    return await parser.parseURL('https://medium.com/feed/appvia');
-  } catch (err) {
-    return {items: []}
+//todo: 
+// - put token in env configs
+// - refactor in to blog routes
+
+
+async  function apiRequest(path, filter){
+    try {
+      console.log(apiBase + path + '?' + filter + '&token='+token)
+      return await superagent.get(apiBase + path + '?' + filter + '&token='+token)
+    } catch (err) {ß
+      return {items: []}
   }
 }
-
-async  function getBlog() {
-  try {
-    return await superagent.get('http://localhost:1337/blogposts')
-  } catch (err) {
-    return {items: []}
-  }
-}
-
-async  function getBlogPost(slug) {
-  try {
-    return await superagent.get('http://localhost:1337/blogposts?slug='+slug)
-  } catch (err) {
-    return {items: []}
-  }
-}
-
-async  function getBlogTaggedPosts(tag) {
-  try {
-    return await superagent.get('http://localhost:1337/blogposts?tags.tag='+tag)
-  } catch (err) {
-    return {items: []}
-  }
-}
-
 
 router.get('/', function (req, res) {
   res.render('index.html', {title: 'Appvia: Home', hubDemoEnabled});
@@ -58,21 +41,24 @@ router.get('/products/hub', function (req, res) {
   res.render('hub.html', {title: 'Appvia: Hub'});
 });
 
-router.get('/products/product', function (req, res) {
-  res.render('product.html', {title: 'Appvia: Product'});
+router.get('/products/kubernetes-security-scanner', function (req, res) {
+  res.render('kubernetes-security-scanner.html', {title: 'Appvia: Product'});
 });
 
+router.get('/services', function (req, res) {
+  res.render('services.html', {title: 'Appvia: Services'});
+});
 
 router.get('/blog', async function (req, res) {
-  res.render('blog.html', {title: 'Appvia: Blog', rss: await getBlog()});
+  res.render('blog.html', {title: 'Appvia: Blog', rss: await apiRequest('stories', '') });
 });
 
 router.get('/blog/:blogpost', async function (req, res) {
-  res.render('blog-post.html', {title: 'Appvia: Blog', rss: await getBlogPost(req.params.blogpost)});
+  res.render('blog-post.html', {title: 'Appvia: Blog', rss: await  apiRequest('stories/blog/'+req.params.blogpost, '') });
 });
 
 router.get('/blog/tag/:tag', async function (req, res) {
-  res.render('blog-tags.html', {title: 'Appvia: Blog', rss: await getBlogTaggedPosts(req.params.tag), tag: req.params.tag});
+  res.render('blog-tags.html', {title: 'Appvia: Blog', rss: await  apiRequest('stories/', 'with_tag='+req.params.tag), tag: req.params.tag });
 });
 
 router.get('/careers', function (req, res) {
