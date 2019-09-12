be20636456b370a6e69563a6544e4c759f8552db
Alecsa <sommes.email@gmail.com>
2019-09-12 18:36:06 +0100

    one commit on new-website-design

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,47 @@ var express = require('express');
 var Parser = require('rss-parser');
 var parser = new Parser();
 var router = express.Router();
+const superagent = require('superagent');
 
 const hubDemoEnabled = process.env.HUB_DEMO_ENABLED === 'true';
+
 const jobs = require('../jobs').filter(j => j.active);
 
-async function getBlogFeed() {
-  try {
-    return await parser.parseURL('https://medium.com/feed/appvia');
-  } catch (err) {
-    return {items: []}
+const apiBase = 'https://api.storyblok.com/v1/cdn/';
+const token = 'pktLzISg00u5e33rrefWPgtt';
+
+//todo: 
+// - put token in env configs
+// - refactor in to blog routes
+
+
+async  function apiRequest(path, filter){
+    try {
+      console.log(apiBase + path + '?' + filter + '&token='+token)
+      return await superagent.get(apiBase + path + '?' + filter + '&token='+token)
+    } catch (err) {ß
+      return {items: []}
   }
 }
 
 router.get('/', function (req, res) {
-  res.render('index.html', {title: 'Appvia: Home'});
+  res.render('index.html', {title: 'Appvia: Home', hubDemoEnabled});
 });
 
-router.get('/about', function (req, res) {
-  res.render('about.html', {title: 'Appvia: About'});
+router.get('/why-appvia', function (req, res) {
+  res.render('why-appvia.html', {title: 'Appvia: Why Appvia'});
 });
 
 router.get('/products', function (req, res) {
-  res.render('products.html', {title: 'Appvia: Products', hubDemoEnabled});
+  res.render('products.html', {title: 'Appvia: Products'});
+});
+
+router.get('/products/hub', function (req, res) {
+  res.render('hub.html', {title: 'Appvia: Hub'});
+});
+
+router.get('/products/kubernetes-security-scanner', function (req, res) {
+  res.render('kubernetes-security-scanner.html', {title: 'Appvia: Product'});
 });
 
 router.get('/services', function (req, res) {
@@ -31,29 +50,33 @@ router.get('/services', function (req, res) {
 });
 
 router.get('/blog', async function (req, res) {
-  res.render('blog.html', {title: 'Appvia: Blog', rss: await getBlogFeed()});
+  res.render('blog.html', {title: 'Appvia: Blog', rss: await apiRequest('stories', '') });
+});
+
+router.get('/blog/:blogpost', async function (req, res) {
+  res.render('blog-post.html', {title: 'Appvia: Blog', rss: await  apiRequest('stories/blog/'+req.params.blogpost, '') });
+});
+
+router.get('/blog/tag/:tag', async function (req, res) {
+  res.render('blog-tags.html', {title: 'Appvia: Blog', rss: await  apiRequest('stories/', 'with_tag='+req.params.tag), tag: req.params.tag });
 });
 
 router.get('/careers', function (req, res) {
-  res.render('careers.html', {title: 'Appvia: Careers', jobCount: jobs.length, jobs});
+  res.render('careers.html', {title: 'Appvia: Careers', jobCount: jobs.length, jobs, hubDemoEnabled});
+});
+
+router.get('/contact-us', function (req, res) {
+  res.render('contact-us.html', {title: 'Appvia: Contact Us'});
 });
 
 jobs.forEach(job => {
   router.get(`/careers/${job.slug}`, function (req, res) {
-    res.render('job.html', {title: 'Appvia: Careers', job: job});
+    res.render('job.html', {title: 'Appvia: Careers', job: job, hubDemoEnabled});
   });
 });
 
-router.get('/contact', function (req, res) {
-  res.render('contact.html', {title: 'Appvia: Contact'});
-});
-
 router.get('/privacy-policy', function (req, res) {
-  res.render('privacy-policy.html', {title: 'Appvia: Privacy Policy'});
-});
-
-router.get('/new-s1opdafz3wvqvyv1', function (req, res) {
-  res.render('new.html', {title: 'Appvia'});
+  res.render('privacy-policy.html', {title: 'Appvia: Privacy Policy', hubDemoEnabled});
 });
 
 module.exports = router;
